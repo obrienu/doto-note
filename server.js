6ba250070c1bcd43cdf90cc72e7cbb3c98ac0753
unknown <obrienules@gmail.com>
2020-01-03 17:40:22 +0100

    version 1.0.0

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ app.use("/api/todo", todoRoute);
 
 //serve static file while app is in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "./client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
   });
 }
 
