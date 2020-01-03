2bbc098ba450e98619dba11b2985df7955933038
unknown <obrienules@gmail.com>
2020-01-03 18:23:54 +0100

    version 1.0.0

--- server.js
+++ server.js
@@ -17,17 +17,15 @@ mongoose
 
 //Get Items Route
 app.use("/api/todo", todoRoute);
-console.log(process.env);
-
-app.use(express.static(path.join(__dirname, "/client/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
-});
 
 //serve static file while app is in production
-//if (process.env.NODE_ENV === "development") {
-//}
+if (process.env.NODE_ENV === "development") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
+  });
+}
 
 const port = process.env.PORT || 5050;
 
